e3a9608290696129b9c10e64148c2fbef847dc43
breno-concrete <brenocount@gmail.com>
2026-04-02 13:13:30 -0300

    Feat: implementing new classes

---
+++ src/main/java/com/breno/marketplace_test/models/ShoppingCart.java
@@ -0,0 +1,27 @@
+package com.breno.marketplace_test.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "carts")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+}
