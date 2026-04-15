1bd7b866ca1e9ee7e30c8f3d3a1ce11a2caa4b77
breno-concrete <brenocount@gmail.com>
2026-04-15 13:15:21 -0300

    feat(security): implement roles on user class

---
+++ src/main/java/com/breno/marketplace_test/models/UserRole.java
@@ -0,0 +1,16 @@
+package com.breno.marketplace_test.models;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role){
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
